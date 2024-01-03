4ea55fc242688a64f01933cc5016ec1e1e35e0c7
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-03 12:01:34 -0500

    Write repeatString function such that it passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = ""
+    for (let i = 1; i <= num; i++) {
+        result += str;
+    } if (num < 0) {
+        return "ERROR"
+    }
+    return result;
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
