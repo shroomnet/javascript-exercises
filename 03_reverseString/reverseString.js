70c09af916552d2084c517c100122dfbaf6cb73a
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-03 12:15:16 -0500

    Write reverseString function such that it passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("")
+    let reversedArray = splitString.reverse()
+    let reversedString = reversedArray.join("")
+    return reversedString;
 };
 
 // Do not edit below this line
