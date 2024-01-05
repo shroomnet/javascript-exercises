ceb2055521f4e470da106ea4e77ddf7072df7ee6
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-04 23:07:39 -0500

    Write the removeFromArray function such that it passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...args) {
+    // LOOP OVER THE ARGUMENTS
+    for (const arg of args) {
+        // LOOP OVER THE ARRAY
+        for (const item of array) {
+            // GET THE INDEX OF THE CURRENT ARRAY ITEM
+            let index = array.indexOf(item)
+            // IF THE CURRENT ARRAY ITEM MATCHES THE CURRENT ARGUMENT
+            if (item === arg) {
+                // STARTING FROM THE CURRENT INDEX, REMOVE 1 ITEM
+                array.splice(index, 1)
+            }
+        }
+    }
+    return array
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
