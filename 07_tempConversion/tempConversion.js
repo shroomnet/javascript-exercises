2178b60a7399e4eeb3db0947b77925f96e23e985
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-10 16:36:51 -0500

    Write

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let c = 5 / 9 * (f - 32)
+  let rounded = Math.round(c * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let f = (c * 9 / 5) + 32
+  let rounded = Math.round(f * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
