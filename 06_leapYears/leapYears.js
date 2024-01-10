079aadeb44037bc8a58061cd3f869800c87450a1
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-10 16:17:36 -0500

    Write

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,34 @@
-const leapYears = function() {
+// const leapYears = function (year) {
+//     let result
+//     if ((year % 100 && 400 === 0) || (year % 4 === 0)) {
+//         result = true
+//     } else {
+//         result = false
+//     }
+//     return result
+// };
 
+// const leapYears = function (year) {
+//     let isLeapYear = false
+//     if (year % 4 === 0) {
+//         isLeapYear = true
+//     } else if (year % 100 === 0 && year % 400 === 0) {
+//         isLeapYear = true
+//     }
+//     return isLeapYear
+// };
+
+const leapYears = function (year) {
+    let isLeapYear = false
+    if (year % 4 === 0 && year % 100 !== 0) {
+        isLeapYear = true
+    } else if (year % 400 === 0) {
+        isLeapYear = true
+    }
+    return isLeapYear
 };
+
+// console.log(leapYears(1900))
 
 // Do not edit below this line
 module.exports = leapYears;
